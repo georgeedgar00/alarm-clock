a4f7c9212159d15ba578ed79ae0feb6c2fe71d71
George Edgar <georgeedgar00@gmail.com>
2022-11-19 16:39:18 -0600

    new updates

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,26 +1,20 @@
 'use strict';
 
 const display = document.getElementById('clock');
-let alarmTime = null;
+let alarmTime = 0;
 
 function updateTime() {
     const date = new Date();
 
-    const hour = formatTime(date.getHours());
-    const minutes = formatTime(date.getMinutes());
-    const seconds = formatTime(date.getSeconds());
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
 
 
 
     display.innerText=`${hour} : ${minutes} : ${seconds}`
 }
 
-function formatTime(time) {
-    if ( time < 10 ) {
-        return '0' + time;
-    }
-    return time;
-}
 
 function setAlarmTime(value) {
     alarmTime = value;
