b02d2ad45d59f4cf9eeb453c33c036e748250712
George Edgar <georgeedgar00@gmail.com>
2022-11-20 16:34:01 -0600

    style update

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,23 +1,51 @@
 'use strict';
 
 const display = document.getElementById('clock');
-let alarmTime = 0;
+let alarmTime = null;
+let alarmTimeout = null;
 
 function updateTime() {
     const date = new Date();
 
-    const hour = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const hour = formatTime(date.getHours());
+    const minutes = formatTime(date.getMinutes());
+    const seconds = formatTime(date.getSeconds());
 
 
 
     display.innerText=`${hour} : ${minutes} : ${seconds}`
 }
 
+function formatTime(time) {
+    if ( time < 10 ) {
+        return '0' + time;
+    }
+    return time;
+}
 
 function setAlarmTime(value) {
     alarmTime = value;
+}
+
+function setAlarm() {
+    if(alarmTime) {
+        const current = new Date();
+        const timeToAlarm = new Date(alarmTime);
+
+        if (timeToAlarm > current) {
+            const timeout = timeToAlarm.getTime() - current.getTime();
+            alarmTimeout = setTimeout(() => audio.play(), timeout);
+            alert('Alarm set');
+        }
+    }
+}
+
+function clearAlarm() {
+    audio.pause();
+    if (alarmTimeout) {
+        clearTimeout(alarmTimeout);
+        alert('Alarm cleared');
+    }
 }
 
 setInterval(updateTime, 1000);
